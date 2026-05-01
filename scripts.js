57b4eedb9c17e52bf87c12cd5164aed8ed0597d5
angierendon <ar6697@nyu.edu>
2026-04-30 22:03:11 -0400

    remove token

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW5naWUxMjM1MyIsImEiOiJjbWF2aHJnOHQwNGx1MmpwdnMyaXVnZHN0In0.odV43jezFdD0s7CW9uaJXQ';
+mapboxgl.accessToken = MAPBOX_TOKEN;
 
 const map = new mapboxgl.Map({
     container: 'map',
